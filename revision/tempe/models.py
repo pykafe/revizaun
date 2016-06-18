42f3e7215ad2cfb64926556d1006bcc4420f0996
PeteCoward <peter.coward@gmail.com>
2016-06-18 12:11:29 +0900

    adding all the stuff

---
+++ revision/tempe/models.py
@@ -0,0 +1,28 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+
+class District(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __unicode__(self):
+        return 'District {}'.format(self.name)
+
+
+class SubDistrict(models.Model):
+    name = models.CharField(max_length=25)
+    district = models.ForeignKey(District, blank=False, null=False)
+
+    def __unicode__(self):
+        return 'Sub-district {}'.format(self.name)
+
+
+class Suco(models.Model):
+    name = models.CharField(max_length=25)
+    subdistrict = models.ForeignKey(SubDistrict, blank=False, null=False)
+
+    def __unicode__(self):
+        return 'Suco {}'.format(self.name)
---
+++ revision/tempe/views.py
@@ -0,0 +1,15 @@
+from django.views.generic.base import TemplateView
+from  .models import District, SubDistrict, Suco
+
+
+class Index(TemplateView):
+    template_name = 'tempe/index.html'
+
+    def get_context_data(self, *args, **kwargs):
+        ''' Add a list of subcategories by category to the context '''
+
+        context = super(Index, self).get_context_data(*args, **kwargs)
+        context['districts'] = District.objects.all()
+        context['subdistricts'] = SubDistrict.objects.all()
+        context['sucos'] = Suco.objects.all()
+        return context
