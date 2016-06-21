519c58bcdf66d711c70fe5a3bb12f0c92c272826
mariano <dedeus_mariano@yahoo.com>
2016-06-21 10:46:29 +0900

    adding aldeia in models

--- revision/tempe/models.py
+++ revision/tempe/models.py
@@ -26,3 +26,11 @@ class Suco(models.Model):
 
     def __unicode__(self):
         return 'Suco {}'.format(self.name)
+
+
+class Aldeia(models.Model):
+    name = models.CharField(max_length=25)
+    suco = models.ForeignKey(Suco, blank=False, null=False)
+
+    def __unicode__(self):
+        return 'Aldeia {}'.format(self.name)
