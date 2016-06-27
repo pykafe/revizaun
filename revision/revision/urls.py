f74a36f8c975d9772e098cf9459bdf977e82bb15
ony <dejesushonorio03@gmail.com>
2016-06-27 09:13:53 +0900

    add district

--- revision/revision/urls.py
+++ revision/revision/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls import include, url, patterns
+from tempe.views import Index
+from django.conf import settings
 
 urlpatterns = [
     url(r'^', include('tempe.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^$', Index.as_view(), name="index"),
 ]
--- revision/tempe/views.py
+++ revision/tempe/views.py
@@ -1,9 +1,14 @@
 from django.views.generic.base import TemplateView
 from  .models import District, SubDistrict, Suco, Aldeia, Visitor
+from django.views.generic.edit import CreateView
+from django.core.urlresolvers import reverse_lazy
 
 
-class Index(TemplateView):
+class Index(CreateView):
+    model = District
+    fields = ['name']
     template_name = 'tempe/index.html'
+    success_url = reverse_lazy('index')
 
     def get_context_data(self, *args, **kwargs):
         ''' Add a list of subcategories by category to the context '''
@@ -15,3 +20,10 @@ class Index(TemplateView):
         context['aldeias'] = Aldeia.objects.all()
         context['visitors'] = Visitor.objects.all()
         return context
+    
+    def get_form(self, form_class):
+        form = super(Index, self).get_form(form_class)
+        form.fields['name']
+        return form
+    
+    
