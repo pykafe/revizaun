e1c4cb91307607655818bcc6fcf6da4fcc574e75
PeteCoward <peter.coward@gmail.com>
2016-06-30 11:58:16 +0900

    district please add

--- revision/tempe/static/district_store.js
+++ revision/tempe/static/district_store.js
@@ -3,6 +3,8 @@ function DistrictStore() {
     var self = this;
 
     riot.observable(self)
+
+    self.on('district_please', function(){
+        console.log('district_please has been heard by another store');
+    });
 }
-
-
